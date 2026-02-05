888e9f769550ec9eae252e0debf6014b2956ab13
Chinmay Mehrotra <88617477+chinmay3012@users.noreply.github.com>
2026-02-06 02:26:27 +0530

    Implement flatten function for binary tree

--- Trees/src/DFS.java
+++ Trees/src/DFS.java
@@ -18,3 +18,23 @@ class Solution {
 }
 
 
+// LC- Flatten Binary Tree to Linked List
+
+class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode curr = root;
+
+        while(curr != null){
+            if(curr.left != null){
+                TreeNode temp = curr.left;
+                while(temp.right != null){
+                    temp = temp.right;
+                }
+                temp.right = curr.right ;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+            curr = curr.right;
+        }
+    }
+
