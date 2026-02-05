b4cd25bc8180903d73e28cfc4befcf6d39fb1736
Chinmay Mehrotra <88617477+chinmay3012@users.noreply.github.com>
2026-02-06 02:28:04 +0530

    Implement invertTree method in DFS.java
    Added a solution for inverting a binary tree.

--- Trees/src/DFS.java
+++ Trees/src/DFS.java
@@ -38,3 +38,20 @@ class Solution {
         }
     }
 
+
+    // LC - Invert Binary Tree
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right ;
+        root.right = left;
+        return root;
+    }
+}
+
