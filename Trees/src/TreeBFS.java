048ee639083770d866adb5ccdbca292be79516f6
Chinmay Mehrotra <mehrotrachinmay4@gmail.com>
2026-02-11 01:22:24 +0530

    LC_230

--- Trees/src/TreeBFS.java
+++ Trees/src/TreeBFS.java
@@ -195,6 +195,32 @@ public class TreeBFS {
             }
         }
 
+        //230. Kth Smallest Element in a BST
+        class Solution {
+            int count =0;
+            public int kthSmallest(TreeNode root, int k) {
+                return helper(root,k).val;
+            }
+
+            public TreeNode helper(TreeNode root , int k){
+                if(root == null){
+                    return null;
+                }
+
+                TreeNode left = helper( root.left,  k);
+                if(left != null){
+                    return left;
+                }
+                count++;
+
+                if(count == k){
+                    return root ;
+                }
+
+                return helper(root.right , k);
+            }
+        }
+
 
     }
 }
